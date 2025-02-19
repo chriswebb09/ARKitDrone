87948f3b5f7b182b77ee620aec5e8b7dd1b73091
Christopher Webb <cdw2146@columbia.edu>
2025-02-18 21:35:48 -0500

    update

--- ARKitDrone/ARKitDrone/Extensions/SCNMatrix4+Extension.swift
+++ ARKitDrone/ARKitDrone/Extensions/SCNMatrix4+Extension.swift
@@ -22,21 +22,3 @@ extension SCNMatrix4 {
             return simd_quatf(rotationMatrix)
         }
 }
-
-//extension float4x4 {
-//    /**
-//     Treats matrix as a (right-hand column-major convention) transform matrix
-//     and factors out the translation component of the transform.
-//     */
-//    var translation: SIMD3<Float> {
-//        get {
-//            let translation = columns.3
-//            return [translation.x, translation.y, translation.z]
-//        }
-//        set(newValue) {
-//            columns.3 = [newValue.x, newValue.y, newValue.z, columns.3.w]
-//        }
-//    }
-//    
-//}
-
