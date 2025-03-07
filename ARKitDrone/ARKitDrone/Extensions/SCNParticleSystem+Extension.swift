48496c52a299605177fe834c705a5d23928b352e
Christopher Webb <cdw2146@columbia.edu>
2025-03-07 13:34:03 -0500

    improve explosion

--- ARKitDrone/ARKitDrone/Extensions/SCNParticleSystem+Extension.swift
+++ ARKitDrone/ARKitDrone/Extensions/SCNParticleSystem+Extension.swift
@@ -12,25 +12,32 @@ extension SCNParticleSystem {
     
     static func createExplosion() -> SCNParticleSystem {
         let explosion = SCNParticleSystem()
-        explosion.emitterShape = SCNSphere(radius: 4)
-        explosion.birthRate = 3000
-        explosion.emissionDuration = 0.15
-        explosion.spreadingAngle = 360
-        explosion.particleLifeSpan = 0.3
-        explosion.particleLifeSpanVariation = 0.2
-        explosion.particleVelocity = 5.0
-        explosion.particleVelocityVariation = 2.5
-        explosion.particleSize = 0.08
-        explosion.particleSizeVariation = 0.05
-        explosion.particleColor = UIColor.orange
-        explosion.particleImage = UIImage(named: "spark")
-        explosion.isAffectedByGravity = false
-        explosion.blendMode = .additive
-        explosion.particleColor = UIColor.orange.withAlphaComponent(1.0)
-        explosion.particleColorVariation = SCNVector4(0.1, 0.1, 0.1, 0.5)
-        explosion.particleLifeSpan = 0.5
+        explosion.emitterShape = SCNSphere(radius: 3.0)
+        explosion.birthRate = 6000
+        explosion.emissionDuration = 0.01
+        explosion.particleLifeSpan = 0.04
         explosion.particleLifeSpanVariation = 0.3
-        explosion.dampingFactor = 1.2
+        explosion.particleVelocity = 15
+        explosion.particleVelocityVariation = 4.0
+        explosion.spreadingAngle = 360
+        explosion.particleSize = 0.03
+        explosion.particleSizeVariation = 0.05
+        explosion.particleAngularVelocity = 5.0
+        explosion.particleAngularVelocityVariation = 5.5
+        explosion.particleColor = UIColor(red: 1, green: 0.6, blue: 0.2, alpha: 1.0)
+        explosion.particleColorVariation = SCNVector4(0.2, 0.2, 0.2, 0.5)
+        let fadeAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        fadeAnimation.values = [1.0, 0.0]
+        fadeAnimation.keyTimes = [0.0, 0.5] as [NSNumber]
+        fadeAnimation.duration = explosion.particleLifeSpan
+        explosion.propertyControllers = [
+            SCNParticleSystem.ParticleProperty.opacity: SCNParticlePropertyController(animation: fadeAnimation)
+        ]
+        explosion.particleImage = UIImage(named: "spark.png")
+        explosion.isAffectedByGravity = true
+        explosion.acceleration = SCNVector3(0, -15.8, 0)
+        explosion.blendMode = .additive
+        explosion.dampingFactor = 0.9
         return explosion
     }
 }
